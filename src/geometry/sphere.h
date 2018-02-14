525d958efe17c19195c8cbcab810606fdb93eb14
Charana <charananandasena@yahoo.com>
2018-02-14 01:21:08 +0000

    opt sphere code, sidenote: serial optimizations are a waste of time

--- src/geometry/sphere.h
+++ src/geometry/sphere.h
@@ -25,41 +25,34 @@ public:
         vec3 dir = normalize(vec3(ray.dir));
         float radius2 = radius * radius;
 
+        //Computing inside of sqrt
         vec3 L = vec3(center) - orig;
         float tca = dot(L,dir);
-        // if (tca < 0) return false;
         float d2 = dot(L,L) - tca * tca; 
-        if (d2 > radius2) return nullptr; 
+        if (d2 > radius2) { //cannot compute sqrt of negative number
+            return nullptr; 
+        }
+
+        //Computing intersection points
         float thc = sqrt(radius2 - d2); 
         float t0 = tca - thc; 
         float t1 = tca + thc; 
-
-        if(thc < 0) { //cannot square root negative number (no intersection)
-            printf("sqrt\n");
-            return nullptr;
+        
+        //Selecting closest intersecting point not beyond the boundary of the cornels box
+        if (t0 > t1) {
+            std::swap(t0, t1); //pick the closer intersection
         }
-        //Analytic solution
-        else {
-            float t0 = tca + thc;
-            float t1 = tca - thc;
-            
-            if (t0 > t1) {
-                std::swap(t0, t1); //pick the closer intersection
+
+        if (t0 < 0) { 
+            t0 = t1; // if t0 is negative, let's use t1 instead 
+            if (t0 < 0) {
+                printf("points outside scene\n");
+                return nullptr; // both t0 and t1 are negative 
             }
- 
-            if (t0 < 0) { 
-                t0 = t1; // if t0 is negative, let's use t1 instead 
-                if (t0 < 0) {
-                    printf("points outside scene\n");
-                    return nullptr; // both t0 and t1 are negative 
-                }
-            } 
-            float t = t0; 
-            //printf("intersection found\n");
-
-            //printf("t[%f] less than length(ray.dir)[%f]\n", t, length(ray.dir));
-            return unique_ptr<vec4>(new vec4(project_to_4D(vec3(ray.start) + (t * dir))));
         }
+        float t = t0; 
+
+        return unique_ptr<vec4>(new vec4(project_to_4D(vec3(ray.start) + (t * dir))));
     }
 
     virtual vec4 compute_normal(vec4 point) const override {
