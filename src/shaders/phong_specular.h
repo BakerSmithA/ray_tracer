921a69f12a1b6ce2d838784a5c96101eb7a16a96
Charana <charananandasena@yahoo.com>
2018-02-19 14:09:33 +0000

    phong

--- src/shaders/phong_specular.h
+++ src/shaders/phong_specular.h
@@ -22,30 +22,21 @@ public:
     // return: the color of the intersected surface, as illuminated by a specific light.
     vec3 color(vec4 position, const Primitive *prim, const Ray &incoming, const Scene &scene, const Light &light) const override {
          
-        //  //Calculate reflection ray direction
-        // vec4 l = light.shadow_ray_to(position).dir;
-        // vec4 v = -incoming.dir;
-        // vec4 lplusv = l + v;
-        // vec4 h = normalize(lplusv); //unit vector
+         //Calculate reflection ray direction
+        vec3 l = vec3(light.shadow_ray_to(position).dir);
+        vec3 surface_normal = normalize(vec3(prim->compute_normal(position)));
+        vec3 reflected_dir = 2.0f * dot(l, surface_normal) * surface_normal - l;
 
-        // //Calculate component of h in the direction of the normal
-        // float proj_prop = dot(h, prim->compute_normal(position));
-        // float proj_prop_scaled_down = glm::pow(proj_prop, dropoff);
+        //Viewing direction
+        vec3 v = -vec3(incoming.dir);
 
+        //Calculate component of viewing direction in the direction of reflected ray
+        float proj_prop = dot(v, reflected_dir);
+        float proj_prop_scaled_down = glm::pow(proj_prop, dropoff);
 
-        // //Calculate reflection ray direction
-        // vec4 surface_normal = prim->compute_normal(position)
-        // vec4 reflected_dir = 2.0f * dot(l, surface_normal) * surface_normal - l;
-        
-        // //Calculate component of viewing direction in the direction of reflected ray
-        // vec4 v = -primary.dir;
-        // float proj_prop = dot(normalize(v), normalize(reflected_dir));
-        // float proj_prop_scaled_down = glm::pow(proj_prop, dropoff);
+        vec3 intensity = light.intensity(position, prim->compute_normal(position));
 
-        // vec3 intensity = light.intensity(position, prim->compute_normal(position));
-
-        // return proj_prop_scaled_down * intensity * this->base_color;
-        return this->base_color;
+        return proj_prop_scaled_down * intensity * this->base_color;
     }
 };
 
