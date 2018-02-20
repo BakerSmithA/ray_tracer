a8d6ac3b80db6d6ed1fd8646fe2d85c512d0dad8
Albie Baker-Smith <bakersmitha@gmail.com>
2018-02-20 00:24:30 +0000

    shows render time

--- src/models/textured_test_model.h
+++ src/models/textured_test_model.h
@@ -95,6 +95,101 @@ Object *ceiling() {
 	return new Object(scaled_triangles(triangles));
 }
 
+// return: the short (red) block in the cornel box.
+Object *short_block() {
+	vec4 A = vec4(290,0,114,1);
+	vec4 B = vec4(130,0, 65,1);
+	vec4 C = vec4(240,0,272,1);
+	vec4 D = vec4(82,0,225,1);
+
+	vec4 E = vec4(290,165,114,1);
+	vec4 F = vec4(130,165, 65,1);
+	vec4 G = vec4(240,165,272,1);
+	vec4 H = vec4(82,165,225,1);
+
+	const vec3 col = vec3(0.75f, 0.15f, 0.15f);
+	const Shader *shader = cornel_shader(col);
+
+	vector<Triangle*> triangles;
+
+	// Front
+	triangles.push_back(new Triangle(E,B,A,shader, 0));
+	triangles.push_back(new Triangle(E,F,B,shader, 0));
+
+	// Front
+	triangles.push_back(new Triangle(F,D,B,shader, 0));
+	triangles.push_back(new Triangle(F,H,D,shader, 0));
+
+	// BACK
+	triangles.push_back(new Triangle(H,C,D,shader, 0));
+	triangles.push_back(new Triangle(H,G,C,shader, 0));
+
+	// LEFT
+	triangles.push_back(new Triangle(G,E,C,shader, 0));
+	triangles.push_back(new Triangle(E,A,C,shader, 0));
+
+	// TOP
+	triangles.push_back(new Triangle(G,F,E,shader, 0));
+	triangles.push_back(new Triangle(G,H,F,shader, 0));
+
+	return new Object(scaled_triangles(triangles));
+}
+
+// return: the tall (blue) block in the cornel box.
+Object *tall_block() {
+	vec4 A = vec4(423,0,247,1);
+	vec4 B = vec4(265,0,296,1);
+	vec4 C = vec4(472,0,406,1);
+	vec4 D = vec4(314,0,456,1);
+
+	vec4 E = vec4(423,330,247,1);
+	vec4 F = vec4(265,330,296,1);
+	vec4 G = vec4(472,330,406,1);
+	vec4 H = vec4(314,330,456,1);
+
+	const vec3 col = vec3(0.15f, 0.15f, 0.75f);
+	const Shader *shader = cornel_shader(col);
+
+	vector<Triangle*> triangles;
+
+	// Front
+	triangles.push_back(new Triangle(E,B,A,shader, 0));
+	triangles.push_back(new Triangle(E,F,B,shader, 0));
+
+	// Front
+	triangles.push_back(new Triangle(F,D,B,shader, 0));
+	triangles.push_back(new Triangle(F,H,D,shader, 0));
+
+	// BACK
+	triangles.push_back(new Triangle(H,C,D,shader, 0));
+	triangles.push_back(new Triangle(H,G,C,shader, 0));
+
+	// LEFT
+	triangles.push_back(new Triangle(G,E,C,shader, 0));
+	triangles.push_back(new Triangle(E,A,C,shader, 0));
+
+	// TOP
+	triangles.push_back(new Triangle(G,F,E,shader, 0));
+	triangles.push_back(new Triangle(G,H,F,shader, 0));
+
+	return new Object(scaled_triangles(triangles));
+}
+
+// return: a glass sphere.
+Object *large_sphere() {
+	vector<Primitive*> primitives;
+	primitives.push_back(new Sphere(vec4(-0, 0.3, -0.9, 1.0), 0.3, new Glass(), 0));
+
+	return new Object(primitives);
+}
+
+Object *small_sphere() {
+	vector<Primitive*> primitives;
+	primitives.push_back(new Sphere(vec4(-0, -0.2, -0.8, 1.0), 0.2, new Glass(), 0));
+
+	return new Object(primitives);
+}
+
 // return: all the objects in the cornel box.
 vector<Object*> objects() {
 	vector<Object*> objects;
@@ -104,6 +199,10 @@ vector<Object*> objects() {
 	objects.push_back(right_wall());
 	objects.push_back(back_wall());
 	objects.push_back(ceiling());
+	// objects.push_back(short_block());
+	// objects.push_back(tall_block());
+	// objects.push_back(large_sphere());
+	// objects.push_back(small_sphere());
 
 	return objects;
 }
