38c7e7410def86244a433d6fd6d62c2b8151b0c5
Albie Baker-Smith <bakersmitha@gmail.com>
2018-03-19 18:50:45 +0000

    Adjusted controls

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@
 #define SCREEN_HEIGHT 256 * 1
 #define FULLSCREEN_MODE false
 #define MAX_NUM_RAY_BOUNCES 3
-#define NUM_SHADOW_RAYS 1
+#define NUM_SHADOW_RAYS 20
 #define NUM_SAMPLES 8
 
 // /*Place updates of parameters here*/
@@ -45,27 +45,27 @@ void update(Camera &camera) {
     // Translate Camera Position
     const uint8_t* scancodes = SDL_GetKeyboardState(NULL);
 
-    const float move_delta = 0.05f;
-    const float yaw_delta = 0.05f;
+    const float move_delta = 0.025f;
+    const float yaw_delta = 0.07f;
 
     if(scancodes[SDL_SCANCODE_DOWN])  camera.move_forward(-move_delta); // Backwards
-    if(scancodes[SDL_SCANCODE_UP])    camera.move_forward(move_delta); // Forwards
-    if(scancodes[SDL_SCANCODE_LEFT])  camera.move_right(-move_delta); // Left
-    if(scancodes[SDL_SCANCODE_RIGHT]) camera.move_right(move_delta); // Right
-    if(scancodes[SDL_SCANCODE_W])     camera.move_down(-move_delta); // Up
-    if(scancodes[SDL_SCANCODE_S])     camera.move_down(move_delta); // Down
-    if(scancodes[SDL_SCANCODE_A])     camera.turn(-yaw_delta);    // Pan Left
-    if(scancodes[SDL_SCANCODE_D])     camera.turn(yaw_delta);     // Pan Right
+    if(scancodes[SDL_SCANCODE_UP])    camera.move_forward(move_delta);  // Forwards
+    if(scancodes[SDL_SCANCODE_A])  camera.move_right(-move_delta);   // Left
+    if(scancodes[SDL_SCANCODE_D]) camera.move_right(move_delta);    // Right
+    if(scancodes[SDL_SCANCODE_W])     camera.move_down(-move_delta);    // Up
+    if(scancodes[SDL_SCANCODE_S])     camera.move_down(move_delta);     // Down
+    if(scancodes[SDL_SCANCODE_LEFT])     camera.turn(-yaw_delta);          // Pan Left
+    if(scancodes[SDL_SCANCODE_RIGHT])     camera.turn(yaw_delta);           // Pan Right
 }
 
 int main(int argc, char* argv[]) {
-    Scene scene = cornel_box();
+    //Scene scene = cornel_box();
     //Scene scene = textured_test_scene();
-    //Scene scene = star_scene();
+    Scene scene = star_scene();
     //Scene scene = saturn_scene();
     //Scene scene = sphere_scene();
-    Camera cam = Camera(vec4(0, 0, -2.3, 1), SCREEN_WIDTH / 2, MAX_NUM_RAY_BOUNCES);
-    //Camera cam = Camera(vec4(0, 0, -1.5, 1), SCREEN_WIDTH / 2, MAX_NUM_RAY_BOUNCES);
+    //Camera cam = Camera(vec4(0, 0, -2.3, 1), SCREEN_WIDTH / 2, MAX_NUM_RAY_BOUNCES);
+    Camera cam = Camera(vec4(0, 0, -1.5, 1), SCREEN_WIDTH / 2, MAX_NUM_RAY_BOUNCES);
     screen *screen = InitializeSDL(SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE);
 
     while (NoQuitMessageSDL()) {
