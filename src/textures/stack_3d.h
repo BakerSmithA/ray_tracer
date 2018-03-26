a521b8f1f68e344c7f7a384120036c259ffd0570
Albie Baker-Smith <bakersmitha@gmail.com>
2018-03-26 17:26:48 +0100

    precalcaultes inverse for stack 3d

--- src/textures/stack_3d.h
+++ src/textures/stack_3d.h
@@ -7,33 +7,27 @@
 // Inspiration from:
 //  See http://shaderbits.com/blog/authoring-pseudo-volume-textures
 class Stack3d: public Texture3d {
-private:
+public:
     // An image containing all the frames. Assumes a square image with the
     // same number of frames on the x- and y-axes.
-    SDL_Surface *frames;
-
-public:
+    const Texture2d *frames;
     // The number of frames on the x- and y-axes.
     const int num_frames_per_side;
+    const float inv_num_frames_per_side;
     // The total number of frames.
     const int num_frames;
-    // The side length of a frame.
-    const int frame_size;
 
-    Stack3d(const char *frames_image_name, const int num_frames_per_side):
-        frames(SDL_LoadBMP(frames_image_name)),
+    Stack3d(const Texture2d *frames, const int num_frames_per_side):
+        frames(frames),
         num_frames_per_side(num_frames_per_side),
-        num_frames(num_frames_per_side * num_frames_per_side),
-        frame_size(this->frames->w / num_frames_per_side)
+        inv_num_frames_per_side(1.0f/(float)num_frames_per_side),
+        num_frames(num_frames_per_side * num_frames_per_side)
     {
-        if (frames == NULL) {
-            printf("Unable to load bitmap: %s\n", SDL_GetError());
-            exit(1);
-        }
     }
 
-    ~Stack3d() {
-        SDL_FreeSurface(this->frames);
+    Stack3d(const char *frames_image_name, const int num_frames_per_side):
+        Stack3d(new File2d(frames_image_name), num_frames_per_side)
+    {
     }
 
     // param position: position in the texture where each axis goes from 0-1.
@@ -41,8 +35,8 @@ public:
     //         coordinate space of the object. The denser this is, the less
     //         light will be allowed to pass through.
     float density_at(vec4 pos) const {
-        // Multiplied by the position to create a pixel position.
-        vec3 m = vec3(this->frame_size, this->frame_size, this->num_frames);
+        // Multiplied by the position to create an frame position.
+        vec3 m = vec3(this->inv_num_frames_per_side, this->inv_num_frames_per_side, this->num_frames);
         // Slices the volume texture along the z-axis.
         vec3 slice_pos_side = vec3(pos) * m;
         // Linearly interpolate the z-axis to avoid slicing artefacts.
@@ -73,13 +67,19 @@ private:
     // return: the density of the pixel at the given x, y, frame_index in the
     //         space of the frames buffer. Therefore the x, y, and frame_index
     //         go from 0 to frames width, height, and frame count respectively.
-    float density_pixel_at(int x, int y, int frame_index) const {
-        // Calculate the position at which the frame (according to frame_index)
-        // starts.
-        int frame_start_y = frame_index / num_frames_per_side;
-        int frame_start_x = frame_index % num_frames_per_side;
+    float density_pixel_at(float x, float y, int frame_index) const {
+        // Convert the 1d frame index into a 2d index.
+        int frame_index_x = frame_index % num_frames_per_side;
+        int frame_index_y = frame_index * this->inv_num_frames_per_side;
 
-        return get_pixel(this->frames, x + frame_start_x * frame_size, y + frame_start_y * frame_size).x;
+        // The origin of the frame in the frames image.
+        float frame_start_x = (float)frame_index_x * this->inv_num_frames_per_side;
+        float frame_start_y = (float)frame_index_y * this->inv_num_frames_per_side;
+
+        //return get_pixel(this->frames, x + frame_start_x / num_frames_per_side, y + frame_start_y / num_frames_per_side).x;
+        //vec2 pos = vec2(x + frame_start_x / num_frames_per_side, y + frame_start_y / num_frames_per_side);
+        vec2 pos = vec2(x + frame_start_x, y + frame_start_y);
+        return this->frames->color_at(pos).x;
     }
 };
 
